d92d48a6453160d4b9cc1c3c55aa5b0e480c02b8
allisonchen23 <achen227@yahoo.com>
2020-04-26 14:53:02 -0500

    created initial server file

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const url = require('url');
+hostname = 'localhost';
+const port = 3000;
+const server = http.createServer((req,res) =>
+{
+    res.setHeader('Content-Type', 'text/plain');
+    let parsed = url.parse(req.url, true);
+    let q = parsed.query;
+    let msg = `At timestamp ${q.time}, the sensor with ID ${q.ID} recorded ${q.vol} liters of water in ${q.dur} seconds.`;
+    res.write(msg);
+    console.log(q);
+    res.end();
+    // if (req.url == '/now')
+    // {
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify({now: new Date() }));
+    // }
+    // else
+    // {
+    //     res.end("hello");
+    // }
+})
+
+
+server.listen(port, hostname, () =>
+{
+    console.log(`Server running at http://${hostname}:${port}`);
+})
