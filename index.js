c55bf8b4090cd03f53e0462d9c35c3f7f7b53ef7
allisonchen23 <achen227@yahoo.com>
2020-04-29 11:55:42 -0500

    added log messages

--- index.js
+++ index.js
@@ -7,23 +7,40 @@ const server = http.createServer((req,res) =>
     res.setHeader('Content-Type', 'text/plain');
     let parsed = url.parse(req.url, true);
     let q = parsed.query;
-    console.log(req.url);
+    date = new Date();
 
     if (q.time && q.ID && q.vol && q.dur)
     {
         let msg = `At timestamp ${q.time}, the sensor with ID ${q.ID} recorded ${q.vol} liters of water in ${q.dur} seconds.\n`;
         res.write(msg);
+        console.log(date + ": " + msg);
     }
     else
     {
+        console.log(date);
         if (!q.time)
+        {
             res.write("invalid time\n");
+            console.log("invalid time ");
+        }
         if (!q.vol)
+        {
             res.write("invalid volume\n");
+            console.log("invalid volume ");
+        }
         if (!q.ID)
+        {
             res.write("invalid ID\n");
+            console.log("invalid ID ");
+
+        }
         if (!q.dur)
+        {
             res.write("invalid dur\n");
+            console.log("invalid dur ");
+
+        }
+        
     }
     res.end();
 })
